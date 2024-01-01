08adc9b7b306acdbb82d7fa44b67cd2aab0fa51c
Nikushaqvavacabaia <nika.kvatsabaia.1@btu.edu.ge>
2024-01-02 00:42:47 +0400

    Authorization function

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,7 @@
 <?php
 use App\Models\Zip;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', "App\Http\Controllers\zipController@getHomePage");
 Route::get('/index', "App\Http\Controllers\zipController@getHomePage");
@@ -21,8 +11,17 @@ Route::get('/tokebi', "App\Http\Controllers\zipController@getTokebiPage");
 Route::get('/ziplines', "App\Http\Controllers\zipController@getZipPage");
 Route::get('/contact', "App\Http\Controllers\zipController@getContactPage");
 
-Route::get('/zip/all', "App\Http\Controllers\zipController@viewAllZip")->name('zips.all');
-Route::post('/zip/add', "App\Http\Controllers\zipController@addNewZip")->name('zips.add');
-Route::post('/zip/delete', "App\Http\Controllers\zipController@deleteZip")->name('zips.delete');
-Route::get('/zip/edit/{id}', "App\Http\Controllers\zipController@editZip")->name('zips.edit');
-Route::post('/zip/update/{id}', "App\Http\Controllers\zipController@updateZip")->name('zips.update');
+Route::middleware('custom-auth')->get('/zip/all', "App\Http\Controllers\zipController@viewAllZip")->name('zips.all');
+Route::middleware('custom-auth')->post('/zip/add', "App\Http\Controllers\zipController@addNewZip")->name('zips.add');
+Route::middleware('custom-auth')->post('/zip/delete', "App\Http\Controllers\zipController@deleteZip")->name('zips.delete');
+Route::middleware('custom-auth')->get('/zip/edit/{id}', "App\Http\Controllers\zipController@editZip")->name('zips.edit');
+Route::middleware('custom-auth')->post('/zip/update/{id}', "App\Http\Controllers\zipController@updateZip")->name('zips.update');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::post('custom/register', [App\Http\Controllers\AuthorizationController::class, 'register'])->name('auth.custom.register')->middleware('guest');
+Route::post('custom/login', [App\Http\Controllers\AuthorizationController::class, 'login'])->name('auth.custom.login')->middleware('guest');
+Route::post('custom/reset-password', [App\Http\Controllers\AuthorizationController::class, 'reset'])->name('auth.custom.reset')->middleware('guest');
+Route::post('custom/logout', [App\Http\Controllers\AuthorizationController::class, 'logout'])->name('auth.custom.logout')->middleware('auth');
