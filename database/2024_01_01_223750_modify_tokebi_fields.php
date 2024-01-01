27c1608dd4b48817dbe8972c03cce1bc9e519098
Nikushaqvavacabaia <nika.kvatsabaia.1@btu.edu.ge>
2024-01-02 02:48:18 +0400

    create Tours / Tokebis Parki / Blogs tables and modify their fields

---
+++ database/2024_01_01_223750_modify_tokebi_fields.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tokebis', function (Blueprint $table) {
+            $table->string('name', 150)->nullable()->change();
+            $table->string('location', 150)->nullable()->change();
+            $table->string('image1', 150)->default("No Image")->change();
+            $table->string('image2', 150)->default("No Image")->change();
+            $table->string('image3', 150)->default("No Image")->change();
+            $table->string('image4', 150)->default("No Image")->change();
+            $table->double('price')->default(0)->change();
+            $table->string('description', 2000)->default("No description")->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
